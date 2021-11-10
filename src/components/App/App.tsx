466715541f520999c7c2266f6bdc575e32517e8c
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-11-10 00:23:02 -0700

    Add isolated modules

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { Error } from '../Error/Error';
 
 export const App: React.FC = () => {
   return (
     <div className="App">
       <p>App</p>
+      <Error />
     </div>
   );
 }
