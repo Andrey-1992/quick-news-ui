f53af1aedce16974146a39cee2d825b59c984c9f
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-11-10 08:30:24 -0700

    Create array with all categories

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,6 +3,24 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 export const NavBar: React.FC = () => {
+  const newsCategory = [
+    {Name:'Select a field', Value:''}, {Name:'Arts', Value:'arts'}, {Name:'Automobiles', Value:'automobiles'}, {Name:'Books', Value:'books'}, 
+    {Name:'Business', Value:'business'}, {Name:'Fashion', Value:'fashion'}, {Name:'Food', Value:'food'},
+    {Name:'Health', Value:'health'}, {Name:'Home', Value:'home'}, 
+    {Name:'Insider', Value:'insider'}, {Name:'Magazine', Value:'magazine'},
+    {Name:'Movies', Value:'movies'}, {Name:'NY Region', Value:'nyregion'},
+    {Name:'Obituaries', Value:'obituaries'}, {Name:'Opinion', Value:'opinion'},{Name:'Politics', Value:'politics'},
+    {Name:'Realstate', Value:'realstate'}, {Name:'Science', Values:'science'}, {Name:'Sports', Value:'sports'}, 
+    {Name:'Sunday Review', Value:'sundayreview'}, {Name:'Technology', Value:'technology'}, {Name:'Theater', Values:'theater'},
+    {Name:'T-Magazine', Value:'t-magazine'}, {Name:'Travel', Value:'travel'}, {Name:'Upshot', Values:'upshot'},
+    {Name:'Us', Value:'us'}, {Name:'World', Value:'world'}
+  ]
+
+    /*
+    arts, automobiles, books, business, fashion, food, health, home, insider, magazine, movies, nyregion, obituaries, opinion, politics, realestate, 
+    science, sports, sundayreview, technology, theater, t-magazine, travel, upshot, us, world
+    */
+
   return (
     <div className="NavBar">
       <NavLink className='go-home' to="/quick-news">
