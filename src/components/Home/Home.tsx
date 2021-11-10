f0ac635cce4ee84054eab8fc3fd7533ddfbdea67
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-11-10 15:55:44 -0700

    Add muktimedia array proptypes

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -10,6 +10,28 @@ interface NewsInfoApi {
   results?: Array<string>
 }
 
+interface Article {
+  section?: string
+  subsection?: string
+  title?: string
+  abstract?: string
+  url?: string
+  uri?: string
+  byline?: string
+  item_type?: string
+  updated_date?: string 
+  created_date?: string
+  published_date?: string
+  material_type_facet?: string
+  kicker?: string
+  des_facet?: Array<string>
+  org_facet?: Array<string>
+  per_facet?: Array<string>
+  geo_facet?: Array<string>
+  multimedia?: Array<string>
+  short_url?: string
+}
+
 interface Props {
   searchedNews?: NewsInfoApi
 }
@@ -17,10 +39,13 @@ interface Props {
 export const Home: React.FC<Props> = ({searchedNews}) => {
   if (searchedNews) {
     console.log(searchedNews)
+    const resultArray = searchedNews.results
+    // const testMap = resultArray.map(news => news.title)
+    console.log(typeof resultArray.section)
     return (
       <div className="Home">
-        <p>Home</p>
-        <p>{searchedNews.status}</p>
+        <p>{searchedNews.copyright}</p>
+
       </div>
     );
   }
