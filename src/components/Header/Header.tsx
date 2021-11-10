6b13492d0c3a117f19637a7c67cd0d24a2d0e26d
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-11-10 07:56:49 -0700

    Add header component to the project

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
 import './Header.css';
+import React from 'react';
+import { NavBar } from '../NavBar/NavBar'
 
 export const Header: React.FC = () => {
+  const currentTime = new Date();
+  console.log(currentTime)
+
   return (
     <div className="Header">
-      <p>Header</p>
+      <div className="project-tittle">
+        <h1>Quick News</h1>
+        {/* <p>{currentTime}</p> */}
+      </div>
+      <NavBar />
     </div>
   );
 }
