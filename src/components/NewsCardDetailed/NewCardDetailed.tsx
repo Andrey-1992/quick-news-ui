cd27e291817d502dc89163adf24af2fd2b4fff87
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-11-10 17:23:43 -0700

    Set up type for card detailed component

--- src/components/NewsCardDetailed/NewCardDetailed.tsx
+++ src/components/NewsCardDetailed/NewCardDetailed.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
 import './NewsCardDetailed.css';
+import React from 'react';
 
-export const NewsCardDetailed: React.FC = () => {
+interface NewsInfoApi {
+  status?: string
+  copyright?: string
+  section?: string
+  last_updated?: string
+  num_results?: number
+  results: Array<Article>
+}
+
+interface Article {
+  section?: string
+  subsection?: string
+  title?: string
+  abstract?: string
+  url?: string
+  uri?: string
+  byline?: string
+  item_type?: string
+  updated_date?: string 
+  created_date?: string
+  published_date?: string
+  material_type_facet?: string
+  kicker?: string
+  des_facet?: Array<string>
+  org_facet?: Array<string>
+  per_facet?: Array<string>
+  geo_facet?: Array<string>
+  multimedia?: Array<string>
+  short_url?: string
+}
+
+interface Props {
+  searchedNews?: NewsInfoApi
+}
+
+export const NewsCardDetailed: React.FC<Props> = ({searchedNews}) => {
   return (
     <div className="NewsCardDetailed">
       <p>NewsCardDetailed</p>
