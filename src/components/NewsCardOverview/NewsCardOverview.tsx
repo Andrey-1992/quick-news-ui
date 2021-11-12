918cfb5ad5bccf92eeaf68d2dfaf34eedaa9e06a
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-11-11 17:51:20 -0700

    Add styling to the button

--- src/components/NewsCardOverview/NewsCardOverview.tsx
+++ src/components/NewsCardOverview/NewsCardOverview.tsx
@@ -47,7 +47,7 @@ export const NewsCardOverview: React.FC<Props> = ({articleNews, changeViewStatus
   }
 
   return (
-    <div className="news-card-detailed" style={{ backgroundImage: `url(${articleNews.multimedia[0].url} height="600")` }}>
+    <div className="news-card-detailed" style={{ backgroundImage: `url(${articleNews.multimedia[0].url})` }}>
       {/* <img src={articleNews.multimedia[0].url} alt={articleNews.multimedia[0].caption} width="500" height="600"></img> */}
       <div className="info-card-overview">
         <h3 className="overview-text">{articleNews.title}</h3>
