609822e33a7088aa6b8161fce4b8c09c7e24b6ea
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-11-11 18:00:56 -0700

    Add relative sizes to the cards

--- src/components/NewsCardOverview/NewsCardOverview.tsx
+++ src/components/NewsCardOverview/NewsCardOverview.tsx
@@ -46,8 +46,14 @@ export const NewsCardOverview: React.FC<Props> = ({articleNews, changeViewStatus
       changeViewStatus('overview', articleNaming)
   }
 
+  const sectionStyle = {
+    width: "100%",
+    height: "400px",
+    // backgroundImage: url(${`articleNews.multimedia[0].url`)
+  };
+
   return (
-    <div className="news-card-detailed" style={{ backgroundImage: `url(${articleNews.multimedia[0].url})` }}>
+    <div className="news-card-detailed" style={{ position: "relative", width: "100%", height: "auto", backgroundImage: `url(${articleNews.multimedia[0].url})` }}>
       {/* <img src={articleNews.multimedia[0].url} alt={articleNews.multimedia[0].caption} width="500" height="600"></img> */}
       <div className="info-card-overview">
         <h3 className="overview-text">{articleNews.title}</h3>
