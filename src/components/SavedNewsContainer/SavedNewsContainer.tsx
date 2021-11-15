4295c47b8c1933b82cd412d99abf938f914fe8a9
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-11-15 15:21:34 -0700

    organize structure for saved news component

--- src/components/SavedNewsContainer/SavedNewsContainer.tsx
+++ src/components/SavedNewsContainer/SavedNewsContainer.tsx
@@ -1,10 +1,13 @@
 import './SavedNewsContainer.css';
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 export const SavedNewsContainer: React.FC = () => {
+  const [ savedNews, setSavedNews ] = useState('')
+
   return (
-    <div className="saved-news-container">
-      <p>Saved News Container</p>
+    <div >
+      <p>hello</p>
     </div>
   );
 }
