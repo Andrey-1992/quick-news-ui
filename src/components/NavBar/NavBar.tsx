e9a8333bcab9401e29a28dedf290489e71ca185b
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-11-15 11:49:47 -0700

    Add background color line

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 import './NavBar.css';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { NewsCategoriesForm } from '../NewsCategoriesForm/NewsCategoriesForm'
 
 interface Props {
@@ -10,12 +10,12 @@ interface Props {
 export const NavBar: React.FC<Props> = ({makeFetch}) => {
   return (
     <div className="nav-bar">
-      <Link className='go-home' to="/">
+      <NavLink className='go-home' to="/">
         <p className="nav-buttons">Home</p>
-      </Link>
-      <Link className='go-saved-news' to="/saved-news">
+      </NavLink>
+      <NavLink className='go-saved-news' to="/saved-news">
         <p className="nav-buttons">Saved News</p>
-      </Link>
+      </NavLink>
       <NewsCategoriesForm makeFetch={makeFetch}/>
     </div>
   );
