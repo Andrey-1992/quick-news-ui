69095c5f84d9095eced7d78abb4f30ccefed4056
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-11-19 12:24:12 -0700

    styling saved cards

--- src/components/NewsCardDetailed/NewsCardDetailed.tsx
+++ src/components/NewsCardDetailed/NewsCardDetailed.tsx
@@ -51,13 +51,13 @@ export const NewsCardDetailed: React.FC<Props> = ({articleNews, changeViewStatus
 
   return (
     <div className="news-card-detailed">
-       <img className="img-card-detailed" src={articleNews.multimedia[0].url} alt={articleNews.multimedia[0].caption} width="500" height="600"></img>
+      <img className="img-card-detailed" src={articleNews.multimedia[0].url} alt={articleNews.multimedia[0].caption} width="500" height="600"></img>
       <h2 className="card-detailed-text">{articleNews.title}</h2>
       <p className="card-detailed-text">{articleNews.abstract}</p>
       <h4 className="card-detailed-text">{articleNews.byline}</h4>
       <p className="card-detailed-text">Category #{articleNews.section}</p>
       <button className='card-detailed-btns'>
-        <a href={articleNews.url} target="_blank">Read Article</a>
+      <a href={articleNews.url} target="_blank">Read Article</a>
       </button>
       <button onClick={saveArticle} className='card-detailed-btns'>Save Article</button>
       <button className='card-detailed-btns' onClick={sendStatus}>Back Home</button>
--- src/components/SavedNewsCard/SavedNewsCard.tsx
+++ src/components/SavedNewsCard/SavedNewsCard.tsx
@@ -1,19 +1,6 @@
 import './SavedNewsCard.css';
 import React from 'react';
 
-// interface savedData {
-//   abstract: string
-//   title: string
-//   byline: string
-//   section: string
-//   imgUrl: string
-//   multimediaUrl: string
-//   multimediaCaption: string
-// }
-// interface Props {
-//   retrivedData: Array<savedData>
-// }
-
 interface Props {
   abstract: string
   title: string
@@ -34,42 +21,24 @@ export const SavedNewsCard: React.FC<Props> = ({
   multimediaCaption
   }) => {
 
-  // const renderData = () => {
-  //   const createdCards =  retrivedData.map(data => {
-  //     return ( 
-  //     // <p>{data.section}</p>
-  //     <p>{data.title}</p>
-  //     // <div className="news-card-detailed">
-  //     //   <img className="img-card-detailed" src={retrivedData.multimediaUrl} alt={retrivedData.multimedia[0].caption} width="500" height="600"></img>
-  //     //   <h2 className="card-detailed-text">{retrivedData.title}</h2>
-  //     //   <p className="card-detailed-text">{retrivedData.abstract}</p>
-  //     //   <h4 className="card-detailed-text">{retrivedData.byline}</h4>
-  //     //   <p className="card-detailed-text">Category #{retrivedData.section}</p>
-  //     //   <button className='card-detailed-btns'>
-  //     //     <a href={retrivedData.imgUrl} target="_blank">Read Article</a>
-  //     //   </button>
-  //     //   <button onClick={deleteArticle} className='card-detailed-btns'>Delete Article</button>
-  //     // </div>
-  //     )
-  //   })
-  //   console.log(createdCards)
-  // }
-
   const deleteArticle = () => {
     console.log('delete gfunctionality would be here')
   }
 
   return (
-    <div className="news-card-detailed">
+    // <div className="news-card-overview" style={{  backgroundSize:"100%", backgroundPosition: "center", backgroundImage: `url(${multimediaUrl})` }}>
+    <div className="news-card-saved">
       <img className="img-card-detailed" src={multimediaUrl} alt={multimediaCaption} width="500" height="600"></img>
-      <h2 className="card-detailed-text">{title}</h2>
-      <p className="card-detailed-text">{abstract}</p>
-      <h4 className="card-detailed-text">{byline}</h4>
-      <p className="card-detailed-text">Category #{section}</p>
-      <button className='card-detailed-btns'>
-      <a href={imgUrl} target="_blank">Read Article</a>
-      </button>
-      <button onClick={deleteArticle} className='card-detailed-btns'>Delete Article</button>
+      {/* <div className="info-card-saved"> */}
+        <h2 className="card-detailed-text">{title}</h2>
+        <p className="card-detailed-text">{abstract}</p>
+        <h4 className="card-detailed-text">{byline}</h4>
+        <p className="card-detailed-text">Category #{section}</p>
+        <button className='card-detailed-btns'>
+        <a href={imgUrl} target="_blank">Read Article</a>
+        </button>
+        <button onClick={deleteArticle} className='card-detailed-btns'>Delete Article</button>
+      {/* </div> */}
     </div>
   );
 }
