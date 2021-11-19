1bf62e385a8e30636ea0b6532285a2038fa79237
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-11-19 11:34:53 -0700

    Set up proptypes for the project

--- src/components/SavedNewsCard/SavedNewsCard.tsx
+++ src/components/SavedNewsCard/SavedNewsCard.tsx
@@ -1,66 +1,41 @@
 import './SavedNewsCard.css';
 import React from 'react';
-interface Article {
-  section?: string
-  subsection?: string
-  title?: string
-  abstract?: string
-  url?: string
-  uri?: string
-  byline?: string
-  item_type?: string
-  updated_date?: string 
-  created_date?: string
-  published_date?: string
-  material_type_facet?: string
-  kicker?: string
-  des_facet?: Array<string>
-  org_facet?: Array<string>
-  per_facet?: Array<string>
-  geo_facet?: Array<string>
-  multimedia: Array<MultimediaInfo>
-  short_url?: string
-}
 
-interface MultimediaInfo {
-  caption: string
-  copyright: string
-  format: string
-  height: number
-  subtype: string
-  type: string
-  url: string
-  width: number
+interface savedData {
+  abstract: string
+  title: string
+  byline: string
+  section: string
+  imgUrl: string
+  multimediaUrl: string
 }
-
 interface Props {
-  articleNews: Article
-  changeViewStatus: (viewStatus: string, articleTitle?: string) => void
-  saveToStorage: () => void
+  retrivedData = Array<savedData>
 }
 
-export const NewsCardDetailed: React.FC<Props> = ({articleNews, changeViewStatus, saveToStorage}) => {
+export const SavedNewsCard: React.FC<Props> = ({retrivedData}) => {
   // console.log(articleNews)
-  const sendStatus = ():void => {
-    changeViewStatus('detailed')
-  }
+  // const sendStatus = ():void => {
+  //   changeViewStatus('detailed')
+  // }
 
-  const saveArticle = (): void => {
-    saveToStorage()
-  }
+  // const saveArticle = (): void => {
+  //   saveToStorage()
+  // }
 
   return (
-    <div className="news-card-detailed">
-       <img className="img-card-detailed" src={articleNews.multimedia[0].url} alt={articleNews.multimedia[0].caption} width="500" height="600"></img>
-      <h2 className="card-detailed-text">{articleNews.title}</h2>
-      <p className="card-detailed-text">{articleNews.abstract}</p>
-      <h4 className="card-detailed-text">{articleNews.byline}</h4>
-      <p className="card-detailed-text">Category #{articleNews.section}</p>
-      <button className='card-detailed-btns'>
-        <a href={articleNews.url} target="_blank">Read Article</a>
-      </button>
-      <button onClick={saveArticle} className='card-detailed-btns'>Save Article</button>
-      <button className='card-detailed-btns' onClick={sendStatus}>Back Home</button>
-    </div>
+    <p>test saved cards</p>
+    // <div className="news-card-detailed">
+    //    <img className="img-card-detailed" src={articleNews.multimedia[0].url} alt={articleNews.multimedia[0].caption} width="500" height="600"></img>
+    //   <h2 className="card-detailed-text">{articleNews.title}</h2>
+    //   <p className="card-detailed-text">{articleNews.abstract}</p>
+    //   <h4 className="card-detailed-text">{articleNews.byline}</h4>
+    //   <p className="card-detailed-text">Category #{articleNews.section}</p>
+    //   <button className='card-detailed-btns'>
+    //     <a href={articleNews.url} target="_blank">Read Article</a>
+    //   </button>
+    //   <button onClick={saveArticle} className='card-detailed-btns'>Save Article</button>
+    //   <button className='card-detailed-btns' onClick={sendStatus}>Back Home</button>
+    // </div>
   );
 }
--- src/components/SavedNewsContainer/SavedNewsContainer.tsx
+++ src/components/SavedNewsContainer/SavedNewsContainer.tsx
@@ -2,6 +2,7 @@ import './SavedNewsContainer.css';
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { mockUpData } from './mockUpData';
+import { SavedNewsCard } from '../SavedNewsCard/SavedNewsCard';
 import { Loading } from '../Loading/Loading';
 
 interface savedData {
