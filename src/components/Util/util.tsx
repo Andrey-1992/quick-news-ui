8dc8ec248dd753092881e86c417b2da9c53a65cb
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-01-16 21:36:14 -0600

    Remove env unsed code

--- src/components/Util/util.tsx
+++ src/components/Util/util.tsx
@@ -1,15 +1,3 @@
-import * as dotenv from "dotenv";
-// dotenv.config({ path:'/.env' });
-// require('dotenv').config()
-// import 'dotenv/config'
-//NYT_API_KEY=gFgfPotWyeyYBbaW9U3bbLbBXrropGAx
-// require('dotenv').config({ path: './.env' })
-// require('dotenv').config({path:'path_to_dotenv'});
-// import { ENV_VAR } from "dotenv"
-
-console.log(process.env)
-
-
 export const fetchTopStories = async (newCategory: string) => {
   // const apiKey = process.env.NY_TIMES_API_KEY;
   // console.log(process.env.NY_TIMES_API_KEY)
@@ -20,9 +8,6 @@ export const fetchTopStories = async (newCategory: string) => {
     }
     const data = await response.json()
     return data
-    // const dataResults = await data.results
-    // return dataResults
-    // console.log(dataResults)
   } catch(err) {
     return err;
   }
