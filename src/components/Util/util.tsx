efbb1f59a32eca166eeb924e5887e4096a9214fb
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-01-18 09:16:53 -0600

    Test with local host cors access the saved news route

--- src/components/Util/util.tsx
+++ src/components/Util/util.tsx
@@ -13,7 +13,8 @@ export const fetchTopStories = async (newCategory: string) => {
 
 export const getSavedNews = async () => {
   try {
-    const response = await fetch('https://quick-news-api.herokuapp.com/saved-news')
+    // const response = await fetch('https://quick-news-api.herokuapp.com/saved-news')
+    const response = await fetch('http://localhost:4000/saved-news')
     if (!response.ok || response.status === 500) {
       throw Error('Error fetching news top stories')
     }
