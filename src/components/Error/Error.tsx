23aa0e818ab5a97d39fd6bea358001a73548bd89
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-02-04 19:17:07 -0600

    Add and fix route in the error img of the project

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom';
 export const Error: React.FC = () => {
   return (
     <div className='error'>
-      <NavLink to='/suspicious-ip'>
+      <NavLink to='/'>
         <img className='error-img' src="https://i.pinimg.com/736x/1b/c0/cd/1bc0cd2ef1a060eebb3deb5d55ecf2d6.jpg" alt="404-img"/>
       </NavLink>
     </div>
